6aefa28d93857244b1d7004e14c6b34bb3d34f48
astrolindh <slagverkare.astrid.lindh@gmail.com>
2022-01-28 18:32:03 +0100

    setup

---
+++ hw1/matrix.c
@@ -0,0 +1,17 @@
+#ifndef _REENTRANT 
+#define _REENTRANT 
+#endif 
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
+#include <sys/time.h>
+
+#define MAXSIZE 10000  /* maximum matrix size */
+#define MAXWORKERS 10   /* maximum number of workers */
+
+pthread_mutex_t barrier;  /* mutex lock for the barrier */
+pthread_cond_t go;        /* condition variable for leaving */
+int numWorkers;           /* number of workers */ 
+int numArrived = 0;       /* number who have arrived */
