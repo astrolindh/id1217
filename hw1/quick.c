41c559e92fa172e1c8408c4b51909feef3ca493a
astrolindh <slagverkare.astrid.lindh@gmail.com>
2022-02-01 22:46:52 +0100

    hw1 task 2 påbörjad. quick.c sekventiell sortering

--- hw1/quick.c
+++ hw1/quick.c
@@ -1,12 +1,8 @@
+
 /*
-    Attempt at paralellizing quicksort, using recursive paralellism.
+Enkel implementation av quicksort, sekventiell
 */
 
-#ifndef _REENTRANT
-#define _REENTRANT
-#endif
-
-#include <pthread.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
@@ -24,8 +20,7 @@ int size;                      // n values to be sorted
 int num_workers;            // number of workers
 int unsorted[MAXSIZE];
 
-
-// timer for paralell code section
+// timer
 double read_timer(){
     static bool initialized = false;
     static struct timeval start;
@@ -38,44 +33,46 @@ double read_timer(){
     return (end.tv_sec - start.tv_sec) + 1.0e-6 * (end.tv_usec - start.tv_usec);
 }
 
-
-void *Quick(void *arg, int numbers[], int first, int last){
-    long myid = (long) arg;
+void quick(int values[], int first, int last){
     int i, j, pivot, temp;
-
-    printf("worker %ld is starting\n", myid);
+    if(first<last){
+        pivot = first;
+        i = first;
+        j = last;
+        while(i < j){
+            while(values[i] <= values[pivot] && i < last) { i++; }
+            while(values[j] > values[pivot]) { j--; }
+            if(i < j){
+                temp = values[i];
+                values[i] = values[j];
+                values[j] = temp;
+            }
+        }
+        temp = values[pivot];
+        values[pivot] = values[j];
+        values[j] = temp;
+        quick(values, first, j-1);  // sort left subsection
+        quick(values, j+1, last);   // sort right subsection
+    }
 }
 
 int main(int argc, char *argv[]){
     // read input arguments from command line, or set to standard values
     size = (argc > 1)? atoi(argv[1]) : STANDARDSIZE;
     if(size > MAXSIZE){size = STANDARDSIZE;}
-    num_workers = (argc > 2)? atoi(argv[2]) : STANDARDWORKERS;
-    if(num_workers > MAXWORKERS){num_workers = STANDARDWORKERS;}
-
-    pthread_attr_t attr;
-    pthread_t workers[num_workers];
-    long w = 0;
-    pthread_attr_init(&attr);
-    pthread_attr_setschedpolicy(&attr, PTHREAD_SCOPE_SYSTEM);
 
     // create the array to be sorted, populate with values of [0,98]
     for(int i = 0; i <= size; i++){ unsorted[i] = rand()%99; }
-    for(long j = 0; j <= num_workers; j++){ workers[j]; }
     #ifdef DEBUG
         printf("{");
-        for(int i = 0; i < size; i++){
+        for(int i = 0; i <= size; i++){
             printf("%d, ", unsorted[i]);
         }
         printf("}\n");
     #endif
+
     start_time = read_timer();
-
-    long w2 = 1;
-    pthread_create(&workers[w], &attr, Quick, (void *) w);
-    pthread_create(&workers[w2], &attr, Quick, (void *) w2);
-
-
+    quick(unsorted, 0, size -1);
     end_time = read_timer();
     #ifdef DEBUG
         printf("{");
@@ -84,6 +81,5 @@ int main(int argc, char *argv[]){
         }
         printf("}\n");
     #endif
-    printf("Worker %ld finished\n", w);
     printf("The execution time is %g seconds\n", end_time - start_time);
 }
