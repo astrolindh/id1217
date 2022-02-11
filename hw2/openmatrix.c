987e9212096a83cd660247ccebbf23dfb2f557ab
astrolindh <slagverkare.astrid.lindh@gmail.com>
2022-02-11 16:30:52 +0100

    hw2 task1 ser ut att funka, ska testa mer

--- hw2/openmatrix.c
+++ hw2/openmatrix.c
@@ -19,6 +19,13 @@ int matrix [MAXSIZE][MAXSIZE];
 void *Worker(void *);           // placeholder procedure
 
 
+// stores max or min values with index position
+struct extreme_val{
+    int value;
+    int row;
+    int col;
+};
+
 int main(int argc, char *argv[]){
     int m, n, total = 0;
     // attempt to read command line arguments, check that arguments are within bounds
@@ -30,25 +37,104 @@ int main(int argc, char *argv[]){
     omp_set_num_threads(num_workers);
 
     // initialize matrix
-
+    int modulo = 99;
     for(m = 0; m < size; m++){
         for(n = 0; n < size; n++){
-            matrix[m][n] = rand()%99;
+            matrix[m][n] = rand()%modulo;
         }
     }
 
+    #ifdef DEBUG
+        printf("[");
+        for(m = 0; m < size; m++){
+            for(n = 0; n < size; n++){
+                printf("%d, ", matrix[m][n]);
+            }
+            printf("\n");
+        }
+        printf("]\n");
+    #endif
+    
+
+    struct extreme_val maxs[num_workers];
+    struct extreme_val mins[num_workers];
+
+    struct extreme_val max = {-1, 0, 0};
+    struct extreme_val min = {modulo, 0, 0};  
+
+    for(int i = 0; i < num_workers; i++){
+        maxs[i] = max;
+        mins[i] = min;
+    }
+
+    int local_maxs[num_workers];
+    int local_mins[num_workers];
+    for(int i = 0; i < num_workers; i++){
+        local_maxs[i] = 0;
+        local_mins[i] = modulo;
+    }
+
+
+    // struct extreme_val max = {-1, 0, 0};
+    // struct extreme_val min = {modulo, 0, 0};
     start_time = omp_get_wtime();
-    #pragma omp parallell for reduction (+:total) private(j)
+
+    // TODO kan vi dela upp så att vi får en nästling? först en parallell, för att hämta min, max. Sen en for reduction, flr summeringen?
+
+      
+    #pragma omp parallell for reduction (+:total) private(n);
     for(m  = 0; m < size; m++){
         for(n = 0; n < size; n++){
-            total +=matrix[m][n];
+            int a = matrix[m][n];
+            total += a;
+
+
+            if(local_maxs[omp_get_thread_num()] < a){
+                local_maxs[omp_get_thread_num()] = a;
+                struct extreme_val ext = {a, m, n};
+                maxs[omp_get_thread_num()] = ext;
+            }
+            if(local_mins[omp_get_thread_num()] > a){
+                local_mins[omp_get_thread_num()] = a;
+                struct extreme_val ext = {a, m, n};
+                mins[omp_get_thread_num()] = ext;
+            }
         }
     }
     // end pragma, implicit barrier
 
     end_time = omp_get_wtime();
     printf("the total is %d\n", total);
+    // extract, max, min values
+    int lowest = modulo;
+    int highest = -1;
+
+    int index_max, index_min;
+    if(num_workers < 2)
+    {
+        index_min = 0;
+        index_max = 0;
+    }
+    else{
+        for(int i = 0; i < num_workers; i++){
+            if(maxs[i].value > highest)
+            {
+                highest = maxs[i].value;
+                index_max = i;
+            }
+            if(mins[i].value < lowest){
+                lowest = mins[i].value;
+                index_min;
+            }
+        }
+    }
+
+    min = mins[index_min];
+    max = maxs[index_max];
+    printf("min value %d found at [%d, %d]\n", min.value, min.row, min.col);
+    printf("max value %d found at [%d, %d]\n", max.value, max.row, max.col);
     printf("executed in %g seconds, matrix size of %d and %d threads\n", end_time - start_time, size, num_workers);
+
     
 }
 
