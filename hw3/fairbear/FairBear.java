abb908b5754825ac89c5c1979ef696e57497de1c
Astrid Lindh <slagverkare.astrid.lindh@gmail.com>
2022-02-17 23:36:33 +0100

    hw3 task2 funkar, fairbear påbörjad för att bättre visa på effekter av semaphor utan FIFO-kö

--- hw3/fairbear/FairBear.java
+++ hw3/fairbear/FairBear.java
@@ -1,14 +1,9 @@
 /*
-Given are n honeybees and a hungry bear. They share a pot of honey.
-The pot is initially empty; its capacity is H portions of honey. The bear sleeps until the pot is full, then eats all
-the honey and goes back to sleep. Each bee repeatedly gathers one portion of honey and puts it in the pot;
-the bee who fills the pot awakens the bear.
+Homework assignement 3, ID1217 VT22
+Astrid Lindh
 
-Develop and implement a multithreaded program to simulate the actions of the bear and honeybees.
-Represent the bear and honeybees as concurrent threads (i.e. a "bear" thread and an array of "honeybee" threads),
-and the pot as a critical shared resource that can be accesses by at most one thread at a time.
-Use only semaphores for synchronization. Your program should print a trace of interesting simulation events.
-Is your solution fair (w.r.t. honeybees)? Explain when presenting homework.
+An extension of HungryBear, with the aim to better prove fairness among the producer threads (ProducerBees) in a
+more effective way than printing traces along the execution
  */
 
 import java.util.concurrent.*;
@@ -122,7 +117,7 @@ class ProducerBee extends Thread{
 
 
 // driver class
-public class HungryBear{
+public class FairBear{
     public static void main(String args[]) {
         // TODO fill out
         int nBees, potCapacity;
